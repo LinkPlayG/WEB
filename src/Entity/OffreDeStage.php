0601c6fda8b2b67daba2d0f01b47aa0da7df6ad6
Majcher <vladimir.majcher@viacesi.fr>
2025-04-03 20:55:04 +0200

    css new_etudiant et pilote

--- src/Entity/OffreDeStage.php
+++ src/Entity/OffreDeStage.php
@@ -9,6 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: OffreDeStageRepository::class)]
+#[ORM\Table(name: "offre_de_stage")]
 class OffreDeStage
 {
     #[ORM\Id]
